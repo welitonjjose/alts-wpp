73c08642784893adcfe2233668d945188b5529d0
Weliton Jose <welitonj.jose@gmail.com>
2026-01-14 15:41:03 -0300

    migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,7 @@ echo "Database ready to accept connections."
 
 #install missing gems for local dev as we are using base image compiled for production
 bundle install
+bundle exec rails db:migrate db:seed
 
 BUNDLE="bundle check"
 
